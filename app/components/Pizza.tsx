07e335f2d98a008fd3b8ae1f70bdc3e311055477
Israel Kilday <israel.kilday@yahoo.com.br>
2024-09-02 22:55:18 -0300

    feat: add description , name and price in component Pizza

--- app/components/Pizza.tsx
+++ app/components/Pizza.tsx
@@ -27,8 +27,34 @@ const Pizza: React.FC<PizzaProps> = ({ pizza }) => {
   console.log(pizza);
 
   return (
-    <div>
-      <Image src={pizza.image} width={270} height={270} alt="pizzas Images" />
+    <div className="group rounded-xl px-4 py-2 xl:px-2 xl:py-4">
+      <Image
+        src={pizza.image}
+        width={270}
+        height={270}
+        alt="pizzas Images"
+        priority={true}
+      />
+
+      {/* title */}
+      <div>
+        <div className="mb-3 cursor-pointer text-xl font-bold capitalize">
+          {pizza.name}
+        </div>
+      </div>
+
+      {/* description */}
+      <div className="mb-6 min-h-[60px] bg-pink-200 text-sm font-medium">
+        {pizza.description}
+      </div>
+
+      {/* price & btn  */}
+      <div>
+        {/* price -> hidden (sm) - visible (lg) */}
+        <div className="hidden text-xl font-semibold lg:flex">
+          stars at {pizza.priceSm}
+        </div>
+      </div>
     </div>
   );
 };
