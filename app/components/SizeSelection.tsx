6bfa97b0eaf33b0ca6ef519b605d3b5cdb9d635a
Israel Kilday <israel.kilday@yahoo.com.br>
2024-09-08 22:08:14 -0300

    feat: add image on label in SizeSelection.tsx

--- app/components/SizeSelection.tsx
+++ app/components/SizeSelection.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+import Image from "next/image";
 import { SizeSelectionProps } from "../types/sizeSelection";
 
 const SizeSelection: React.FC<SizeSelectionProps> = ({
@@ -6,7 +7,30 @@ const SizeSelection: React.FC<SizeSelectionProps> = ({
   size,
   setSize,
 }) => {
-  return <div>SizeSelection</div>;
+  return (
+    <div className="bg-indigo-300">
+      {/* sizes */}
+      <div>
+        <label>
+          <Image
+            src={pizza.image}
+            width={60}
+            height={60}
+            alt="pizza_iamge_label"
+          />
+
+          <input
+            type="radio"
+            name="size"
+            value="small"
+            checked={size === "small"}
+            onChange={(e) => setSize(e.target.value)}
+            className="appearance-none"
+          />
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default SizeSelection;
