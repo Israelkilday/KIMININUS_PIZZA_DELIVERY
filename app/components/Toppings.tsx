9155736f4fc346274e1de8b297591a38dbf7d6e7
Israel Kilday <israel.kilday@yahoo.com.br>
2024-09-10 20:00:23 -0300

    style: adjust checkbox in topping.tsx

--- app/components/Toppings.tsx
+++ app/components/Toppings.tsx
@@ -2,17 +2,19 @@
 import Image from "next/image";
 import { ToppingProps } from "../types/Topping";
 import { useState } from "react";
+import { IoMdCheckmark } from "react-icons/io";
 
 const Toppings: React.FC<ToppingProps> = ({
   topping,
   additionalTopping,
   setAdditionalToppinPrice,
 }) => {
+  // checkbox state
   const [isChecked, setTsChecked] = useState(false);
 
   return (
     <div
-      className={`${isChecked && "border-orange"} h-[140px] w-full max-w-[110px] p-1`}
+      className={`${isChecked && "border-orange"} relative flex h-[140px] w-full max-w-[110px] flex-col items-center rounded-md border bg-white p-1`}
     >
       <Image
         src={topping.image}
@@ -25,6 +27,18 @@ const Toppings: React.FC<ToppingProps> = ({
       {/* topping name */}
       <div className="text-center text-sm font-medium capitalize">
         {topping.name}
+      </div>
+
+      {/* checkbox */}
+      <input
+        type="checkbox"
+        checked={isChecked}
+        className="absolute h-full w-full cursor-pointer opacity-0"
+      />
+
+      {/* checkmark icon */}
+      <div className={`${isChecked ? "opacity-100" : "opacity-0"}`}>
+        <IoMdCheckmark className="text-xl text-orange" />
       </div>
     </div>
   );
