c239c098f49412e1c8727b66548493666db6c577
Israel Kilday <israel.kilday@yahoo.com.br>
2024-09-10 13:32:50 -0300

    feat: add CrustSelection.tsx

---
+++ app/components/CrustSelection.tsx
@@ -0,0 +1,8 @@
+import { CrustSelectionProps } from "../types/CrustSelection";
+
+// eslint-disable-next-line no-unused-vars
+const CrustSelection: React.FC<CrustSelectionProps> = ({ crust }) => {
+  return <div>CrustSelection</div>;
+};
+
+export default CrustSelection;
