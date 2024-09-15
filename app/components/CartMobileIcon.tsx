83bfe028114256d784c722b38a4dcaddda42ba64
Israel Kilday <israel.kilday@yahoo.com.br>
2024-09-15 20:39:55 -0300

    feat: add component CartMobileIcon

--- app/components/CartMobileIcon.tsx
+++ app/components/CartMobileIcon.tsx
@@ -1,5 +1,14 @@
+import { BsHandbagFill } from "react-icons/bs";
+
 const CartMobileIcon = () => {
-  return <div>CartMobileIcon</div>;
+  return (
+    <div className="fixed bottom-[5%] right-[10%] z-20 flex h-[72px] w-[72px] cursor-pointer items-center justify-center rounded-full bg-tertiary text-white">
+      <BsHandbagFill className="text-4xl" />
+
+      {/* amount */}
+      <span></span>
+    </div>
+  );
 };
 
 export default CartMobileIcon;
