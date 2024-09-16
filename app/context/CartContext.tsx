7856cb8d4e51541a2fc3a4b77c5ee1135ed274ad
Israel Kilday <israel.kilday@yahoo.com.br>
2024-09-16 10:32:20 -0300

    feat: create custom hook in context

--- app/context/CartContext.tsx
+++ app/context/CartContext.tsx
@@ -5,6 +5,7 @@ import {
   Dispatch,
   ReactNode,
   SetStateAction,
+  useContext,
   useState,
 } from "react";
 
@@ -23,6 +24,16 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
       {children}
     </CartContext.Provider>
   );
+};
+
+export const useCartContext = (): CartContextType => {
+  const context = useContext(CartContext);
+
+  if (!context) {
+    throw new Error("UseCartContext must be userd within a CartProvider");
+  }
+
+  return context;
 };
 
 export default CartProvider;
