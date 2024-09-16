531f41b53028a7dec31c6fefdb2a5913a15ce93a
Israel Kilday <israel.kilday@yahoo.com.br>
2024-09-16 10:59:45 -0300

    feat: add interface function AddTocartFunction

--- app/context/CartContext.tsx
+++ app/context/CartContext.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 "use client";
 
 import {
@@ -8,19 +9,50 @@ import {
   useContext,
   useState,
 } from "react";
+import { Topping } from "../types/pizza";
+
+interface AddToCartFunction {
+  (
+    id: number,
+    image: string,
+    name: string,
+    price: number,
+    additionalTopping: Topping[],
+    size: string,
+    crust: string,
+  ): void;
+}
 
 export interface CartContextType {
   isOpen: boolean;
   setIsOpen: Dispatch<SetStateAction<boolean>>;
+  addToCart: AddToCartFunction;
 }
 
 export const CartContext = createContext<CartContextType | null>(null);
 
 const CartProvider = ({ children }: { children: ReactNode }) => {
+  // cart open state
   const [isOpen, setIsOpen] = useState(false);
 
+  // cart state
+  const [cart, setCart] = useState([]);
+
+  // add to cart
+  const addToCart: AddToCartFunction = (
+    id,
+    image,
+    name,
+    price,
+    additionalTopping,
+    size,
+    crust,
+  ) => {
+    console.log(id, image, name, price, additionalTopping, size, crust);
+  };
+
   return (
-    <CartContext.Provider value={{ isOpen, setIsOpen }}>
+    <CartContext.Provider value={{ isOpen, setIsOpen, addToCart }}>
       {children}
     </CartContext.Provider>
   );
