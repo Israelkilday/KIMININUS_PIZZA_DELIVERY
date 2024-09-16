62c3360c146bf663e3fbaa0753a285c4868e1804
Israel Kilday <israel.kilday@yahoo.com.br>
2024-09-15 21:46:53 -0300

    feat: add context and amount in CartMobileIcon.tsx

--- app/components/CartMobileIcon.tsx
+++ app/components/CartMobileIcon.tsx
@@ -1,12 +1,29 @@
+"use client";
+
+import { useContext } from "react";
 import { BsHandbagFill } from "react-icons/bs";
+import { CartContext } from "../context/CartContext";
 
 const CartMobileIcon = () => {
+  const context = useContext(CartContext);
+
+  if (!context) {
+    return null;
+  }
+
+  const { isOpen, setIsOpen } = context;
+
   return (
-    <div className="fixed bottom-[5%] right-[10%] z-20 flex h-[72px] w-[72px] cursor-pointer items-center justify-center rounded-full bg-tertiary text-white">
+    <div
+      onClick={() => setIsOpen(!isOpen)}
+      className="fixed bottom-[5%] right-[10%] z-20 flex h-[72px] w-[72px] cursor-pointer items-center justify-center rounded-full bg-tertiary text-white"
+    >
       <BsHandbagFill className="text-4xl" />
 
       {/* amount */}
-      <span></span>
+      <span className="gradient absolute bottom-3 right-4 flex h-5 w-5 items-center justify-center rounded-full font-robotoCondensed text-[13px] text-white">
+        3
+      </span>
     </div>
   );
 };
