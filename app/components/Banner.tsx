03881132ea922e648b0eae007817333ae457c6cb
Israel Kilday <israel.kilday@yahoo.com.br>
2024-11-08 22:31:44 -0300

    style: test size slogan in banner

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -20,8 +20,8 @@ const Banner = () => {
             <div className="flex flex-1 flex-col items-center px-6 text-center text-white lg:flex-row lg:text-left">
               <div className="flex-1">
                 <h1 className="mt-20 animate-slide font-bangers text-6xl drop-shadow-md md:mt-10 lg:mt-0 lg:text-[80px]">
-                  O melhor da pizza,
-                  <br /> sem sair de casa!
+                  A verdadeira pizza napolitana,
+                  <br /> onde você estiver!
                 </h1>
               </div>
             </div>
